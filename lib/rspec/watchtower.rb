62b9785556874eeab377d14a8fade7dc248b8e89
Alvaro Almagro <aadoello@gmail.com>
2018-04-14 14:04:02 +0200

    Fix requires

--- lib/rspec/watchtower.rb
+++ lib/rspec/watchtower.rb
@@ -3,11 +3,11 @@ require 'net/http'
 require 'uri'
 require 'json'
 
-require "watchtower/version"
-require 'watchtower/api'
-require 'watchtower/config'
-require 'watchtower/test_run'
-require 'watchtower/test_result'
+require 'rspec/watchtower/version'
+require 'rspec/watchtower/api'
+require 'rspec/watchtower/config'
+require 'rspec/watchtower/test_run'
+require 'rspec/watchtower/test_result'
 
 RSpec::configure do |config|
   config.include(RSpec::Watchtower)
--- rspec-watchtower.gemspec
+++ rspec-watchtower.gemspec
@@ -1,7 +1,7 @@
 
 lib = File.expand_path("../lib", __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
-require "watchtower_rspec/version"
+require "rspec/watchtower/version"
 
 Gem::Specification.new do |spec|
   spec.name          = "watchtower_rspec"
