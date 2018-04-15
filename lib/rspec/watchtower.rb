818926cc171cc310e9a9041d181acb2b604c21c4
Alvaro Almagro <aadoello@gmail.com>
2018-04-15 17:59:40 +0200

    debug

--- lib/rspec/watchtower.rb
+++ lib/rspec/watchtower.rb
@@ -10,17 +10,21 @@ require 'rspec/watchtower/test_run'
 require 'rspec/watchtower/test_result'
 
 RSpec::configure do |config|
+  puts __FILE__
   config.include(RSpec::Watchtower)
 
   config.before(:all) do
+    puts 'before all'
     Watchtower::TestRun.initialize_test_run
   end
 
   config.after(:all) do |example|
+    puts 'after all'
     Watchtower::TestRun.current.submit
   end
 
   config.after do |example|
+    puts 'after'
     Watchtower::TestRun.current.add_test_result(example)
   end
 end
